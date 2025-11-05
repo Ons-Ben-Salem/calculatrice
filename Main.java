ac9a0c7f1e499ccd50ebb10893d3a5adeff97b6e
Ben Salem Ons <bensalem.ons@enis.tn>
2025-11-05 09:00:26 +0000

    Ajout d'un message Bonsoir

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
      public static void main(String[] args) {
           int a=2;
           int b=3;
+          System.out.println("Bonsoir");
           System.out.println("Bienvenue");
           System.out.println(a+"*"+b+"="+ Operation.multiplication(a,b));
           System.out.println(a+"-"+b+"="+ Operation.soustraction(a,b));
